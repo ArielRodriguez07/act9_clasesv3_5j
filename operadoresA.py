16f0aca4755c370afd80a4ca70dad83dbe1f17db
ArielRodriguez07 <a.22308051280706@cbtis128.edu.mx>
2024-09-20 10:00:31 -0600

    Add files via upload

---
+++ operadoresA.py
@@ -0,0 +1,56 @@
+print(" Act 9 clases v3")
+print(" Rodriguez Ariel : 22308051280706")
+class Operadores0706:
+    def suma0706(self,a,b):
+        s0706=a+b
+        print(f"la suma de {a} + {b} = {s0706}")
+
+    def resta0706(self,a,b):
+        s0706=a-b
+        print(f"la resta de {a} + {b} = {s0706}")
+    
+    def multi0706(self,a,b):
+        r0706=a*b
+        print(f"la multi de {a} * {b} = {r0706}")
+    
+    def div0706(self,a,b):
+        d0706=a/b
+        print(f"la division de {a} / {b} = {d0706}")
+    
+    def mod0706(self,a,b):
+        mod0706=a%b
+        print(f"el modulo de {a} % {b} = {mod0706}")
+    
+    def expo0706(self,a,b):
+        expo0706=a*b
+        print(f"el exponente de {a} ** {b} = {expo0706}")
+    
+    def cos0706(self,a,b):
+        cos0706=a//b
+        print(f"el cociente de {a} // {b} = {cos0706}")
+    
+#ZONA DE CREACION DE OBJETOS
+operarodri=Operadores0706()
+
+#ZONA DE USO DE OBJETOS
+print("")
+print("***funcion suma***")
+operarodri.suma0706(5,4)
+print("")
+print("***funcion resta***")
+operarodri.resta0706(10,5)
+print("")
+print("***funcion multiplicacion***")
+operarodri.multi0706(2,8)
+print("")
+print("***funcion division***")
+operarodri.div0706(2,8)
+print("")
+print("***funcion modulo***")
+operarodri.mod0706(8,9)
+print("")
+print("***funcion exponente***")
+operarodri.expo0706(8,5)
+print("")
+print("***funcion cociente***")
+operarodri.cos0706(5,6)
